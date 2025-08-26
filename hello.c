eb3b4b394806652fcc91c16ccf0c5255261ce333
anthony22458 <at22458@gmail.com>
2025-08-26 05:18:00 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+ int main(int argc, char **argv) {
+
+    printf("Hello, I'm Anthony Aviles Truong!\n");
+    printf("Major is Electrical Engineering!\n");
+
+    return 0;
+ }
