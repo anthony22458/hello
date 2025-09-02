a3e6d22f383e4e3298db8d109dad6f0bba0644d2
anthony23738 <jdrdsigisgd@gmail.com>
2025-09-02 04:12:16 +0000

    added my major too

--- hello.c
+++ hello.c
@@ -7,8 +7,9 @@
  int main(int argc, char **argv) {
 
     printf("Hello, I'm Anthony Aviles Truong!\n");
-    printf("Major is Electrical Engineering!\n");
-    printf("Minor is Undecided!\n");
+    printf("Hello, I'm Anthony 2!\n");
+    printf("Our Major are Electrical Engineering!\n");
+    printf("Our Minors are Undecided!\n");
 
     return 0;
  }
